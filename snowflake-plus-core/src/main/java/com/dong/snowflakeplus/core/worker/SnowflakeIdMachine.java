53ce5fbae9235edbc34d2bf050574b8e9d61618d
董世磊 <dongshilei@9fbank.com.cn>
2021-06-23 22:32:54 +0800

    优化代码

--- snowflake-plus-core/src/main/java/com/dong/snowflakeplus/core/worker/SnowflakeIdMachine.java
+++ snowflake-plus-core/src/main/java/com/dong/snowflakeplus/core/worker/SnowflakeIdMachine.java
@@ -7,28 +7,22 @@ package com.dong.snowflakeplus.core.worker;
  * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0<br>
  * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
  * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69<br>
- * 10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId<br>
+ * 10位的数据机器位，可以部署在1024个节点<br>
  * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号<br>
  * 加起来刚好64位，为一个Long型。<br>
  * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
  */
-public class SnowflakeIdWorker {
+public class SnowflakeIdMachine {
 
     // ==============================Fields===========================================
     /** 开始时间截 (2015-01-01) */
     private final long twepoch = 1420041600000L;
 
     /** 机器id所占的位数 */
-    private final long workerIdBits = 5L;
+    private final long machineIdBits = 10L;
 
-    /** 数据标识id所占的位数 */
-    private final long datacenterIdBits = 5L;
-
-    /** 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
-    private final long maxWorkerId = -1L ^ (-1L << workerIdBits);
-
-    /** 支持的最大数据标识id，结果是31 */
-    private final long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+    /** 支持的最大机器id，结果是1023 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
+    private final long maxMachineId = -1L ^ (-1L << machineIdBits);
 
     /** 序列在id中占的位数 */
     private final long sequenceBits = 12L;
@@ -36,20 +30,15 @@ public class SnowflakeIdWorker {
     /** 机器ID向左移12位 */
     private final long workerIdShift = sequenceBits;
 
-    /** 数据标识id向左移17位(12+5) */
-    private final long datacenterIdShift = sequenceBits + workerIdBits;
 
-    /** 时间截向左移22位(5+5+12) */
-    private final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+    /** 时间截向左移22位(10+12) */
+    private final long timestampLeftShift = sequenceBits + machineIdBits;
 
     /** 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095) */
     private final long sequenceMask = -1L ^ (-1L << sequenceBits);
 
-    /** 工作机器ID(0~31) */
-    private long workerId;
-
-    /** 数据中心ID(0~31) */
-    private long datacenterId;
+    /** 机器ID(0~1023) */
+    private long machineId;
 
     /** 毫秒内序列(0~4095) */
     private long sequence = 0L;
@@ -60,18 +49,13 @@ public class SnowflakeIdWorker {
     //==============================Constructors=====================================
     /**
      * 构造函数
-     * @param workerId 工作ID (0~31)
-     * @param datacenterId 数据中心ID (0~31)
+     * @param machineId 机器ID (0~1023)
      */
-    public SnowflakeIdWorker(long workerId, long datacenterId) {
-        if (workerId > maxWorkerId || workerId < 0) {
-            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
+    public SnowflakeIdMachine(long machineId) {
+        if (machineId > maxMachineId || machineId < 0) {
+            throw new IllegalArgumentException(String.format("machine Id can't be greater than %d or less than 0", machineId));
         }
-        if (datacenterId > maxDatacenterId || datacenterId < 0) {
-            throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
-        }
-        this.workerId = workerId;
-        this.datacenterId = datacenterId;
+        this.machineId = machineId;
     }
 
     // ==============================Methods==========================================
@@ -107,8 +91,7 @@ public class SnowflakeIdWorker {
 
         //移位并通过或运算拼到一起组成64位的ID
         return ((timestamp - twepoch) << timestampLeftShift)
-                | (datacenterId << datacenterIdShift)
-                | (workerId << workerIdShift)
+                | (machineId << sequenceBits)
                 | sequence;
     }
 
@@ -136,7 +119,7 @@ public class SnowflakeIdWorker {
     //==============================Test=============================================
     /** 测试 */
     public static void main(String[] args) {
-        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(1, 0);
+        SnowflakeIdMachine idWorker = new SnowflakeIdMachine( 1);
         for (int i = 0; i < 1000; i++) {
             long id = idWorker.nextId();
             System.out.println(Long.toBinaryString(id));
